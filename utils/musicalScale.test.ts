67b4e44facc98c43d4582e455db6ef4a541b1e63
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-04-15 00:00:17 -0300

    musical scale utils

---
+++ utils/musicalScale.test.ts
@@ -0,0 +1,49 @@
+import { generateScale } from "./musicalScales";
+
+test("Scale of root 0 and mode 0 and Natural notes returns C Ionian/Major", () => {
+  expect(generateScale({ root: 0, mode: 0, notes: "natural" })).toStrictEqual([
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "A",
+    "B",
+  ]);
+});
+
+test("Scale of root 11 and mode 0 and Flat notes returns Cb Ionian/Major", () => {
+  expect(generateScale({ root: 11, mode: 0, notes: "flat" })).toStrictEqual([
+    "C♭",
+    "D♭",
+    "E♭",
+    "F♭",
+    "G♭",
+    "A♭",
+    "B♭",
+  ]);
+});
+
+test("Scale of root 11 and mode 1 and Flat notes returns Cb Dorian", () => {
+  expect(generateScale({ root: 11, mode: 1, notes: "flat" })).toStrictEqual([
+    "C♭",
+    "D♭",
+    "D",
+    "F♭",
+    "G♭",
+    "A♭",
+    "A",
+  ]);
+});
+
+test("Scale of root 11 and mode 6 and Flat notes returns Cb Locrian", () => {
+  expect(generateScale({ root: 11, mode: 6, notes: "flat" })).toStrictEqual([
+    "C♭",
+    "C",
+    "D",
+    "F♭",
+    "F",
+    "G",
+    "A",
+  ]);
+});
---
+++ utils/musicalScales.ts
@@ -0,0 +1,31 @@
+interface GenerateScale {
+  root: number;
+  mode: number;
+  notes: string;
+}
+
+type Indexable = {
+  [key: string]: string[];
+};
+
+export function generateScale({ root, mode, notes }: GenerateScale) {
+  if (root < 0 || root > 11 || mode < 0 || mode > 6) return null;
+
+  const ionianSequence = [2, 2, 1, 2, 2, 2, 1];
+  const sequenceEnd = ionianSequence.slice(0, mode);
+  const sequenceStart = ionianSequence.slice(mode, ionianSequence.length);
+  const modeSequence = [...sequenceStart, ...sequenceEnd];
+  const noteMapping: Indexable = {
+    natural: ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"],
+    sharp: ["B♯", "C♯", "D", "D♯", "E", "E♯", "F♯", "G", "G♯", "A", "A♯", "B"],
+    flat: ["C", "D♭", "D", "E♭", "F♭", "F", "G♭", "G", "A♭", "A", "B♭", "C♭"],
+  };
+  let scale = [root];
+
+  for (let i = 0; i < modeSequence.length - 1; i++) {
+    let note = scale[i] + modeSequence[i];
+    scale = [...scale, note < 12 ? note : note - 12];
+  }
+
+  return scale.map((e, k) => noteMapping[notes][scale[k]]);
+}

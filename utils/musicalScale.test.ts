09110e35304317210fdde897c4d0b10ccdad5fb0
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-04-16 00:55:29 -0300

    split note mapping to own function

--- utils/musicalScale.test.ts
+++ utils/musicalScale.test.ts
@@ -1,51 +1,39 @@
-import { generateScale, normalizePitch } from "./musicalScales";
-
-test("Scale of root 0 and mode 0 and Natural notes returns C Ionian/Major", () => {
-  expect(generateScale({ root: 0, mode: 0, notes: "natural" })).toStrictEqual([
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "A",
-    "B",
-  ]);
-});
+import { generateScale, normalizePitch, pitchToNote } from "./musicalScales";
 
 test("Scale of root 11 and mode 0 and Flat notes returns Cb Ionian/Major", () => {
-  expect(generateScale({ root: 11, mode: 0, notes: "flat" })).toStrictEqual([
-    "C♭",
-    "D♭",
-    "E♭",
-    "F♭",
-    "G♭",
-    "A♭",
-    "B♭",
+  expect(generateScale({ root: 11, mode: 0 })).toStrictEqual([
+    11, 1, 3, 4, 6, 8, 10,
   ]);
 });
 
 test("Scale of root 11 and mode 1 and Flat notes returns Cb Dorian", () => {
-  expect(generateScale({ root: 11, mode: 1, notes: "flat" })).toStrictEqual([
-    "C♭",
-    "D♭",
-    "D",
-    "F♭",
-    "G♭",
-    "A♭",
-    "A",
+  expect(generateScale({ root: 11, mode: 1 })).toStrictEqual([
+    11, 1, 2, 4, 6, 8, 9,
   ]);
 });
 
 test("Scale of root 11 and mode 6 and Flat notes returns Cb Locrian", () => {
-  expect(generateScale({ root: 11, mode: 6, notes: "flat" })).toStrictEqual([
-    "C♭",
-    "C",
-    "D",
-    "F♭",
-    "F",
-    "G",
-    "A",
+  expect(generateScale({ root: 11, mode: 6 })).toStrictEqual([
+    11, 0, 2, 4, 5, 7, 9,
   ]);
+});
+
+test("C Major scale gets mapped to correct notes", () => {
+  expect(
+    pitchToNote({ scale: [0, 2, 4, 5, 7, 9, 11], degree: "natural" })
+  ).toStrictEqual(["C", "D", "E", "F", "G", "A", "B"]);
+});
+
+test("Cb Major scale gets mapped to correct notes", () => {
+  expect(
+    pitchToNote({ scale: [11, 1, 3, 4, 6, 8, 10], degree: "flat" })
+  ).toStrictEqual(["C♭", "D♭", "E♭", "F♭", "G♭", "A♭", "B♭"]);
+});
+
+test("C# Major scale gets mapped to correct notes", () => {
+  expect(
+    pitchToNote({ scale: [1, 3, 5, 6, 8, 10, 0], degree: "sharp" })
+  ).toStrictEqual(["C♯", "D♯", "E♯", "F♯", "G♯", "A♯", "B♯"]);
 });
 
 test("Pitch 12 will be normalized to 0", () => {
--- utils/musicalScales.ts
+++ utils/musicalScales.ts
@@ -1,29 +1,20 @@
 interface GenerateScale {
   root: number;
   mode: number;
-  notes: string;
 }
 
 type Indexable = {
-  [key: string]: string[];
+  [key: string]: any;
 };
 
-export function generateScale({
-  root,
-  mode,
-  notes,
-}: GenerateScale): string[] | null {
+export function generateScale({ root, mode }: GenerateScale): number[] | null {
   if (root < 0 || root > 11 || mode < 0 || mode > 6) return null;
 
   const ionianSequence = [2, 2, 1, 2, 2, 2, 1];
   const sequenceEnd = ionianSequence.slice(0, mode);
   const sequenceStart = ionianSequence.slice(mode, ionianSequence.length);
   const modeSequence = [...sequenceStart, ...sequenceEnd];
-  const noteMapping: Indexable = {
-    natural: ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"],
-    sharp: ["B♯", "C♯", "D", "D♯", "E", "E♯", "F♯", "G", "G♯", "A", "A♯", "B"],
-    flat: ["C", "D♭", "D", "E♭", "F♭", "F", "G♭", "G", "A♭", "A", "B♭", "C♭"],
-  };
+
   let scale = [root];
 
   for (let i = 0; i < modeSequence.length - 1; i++) {
@@ -31,7 +22,22 @@ export function generateScale({
     scale = [...scale, note < 12 ? note : note - 12];
   }
 
-  return scale.map((e, k) => noteMapping[notes][scale[k]]);
+  return scale;
+}
+
+interface PitchToNote {
+  scale: number[];
+  degree: string;
+}
+
+export function pitchToNote({ scale, degree }: PitchToNote) {
+  const noteSequences: Indexable = {
+    flat: ["C", "D♭", "D", "E♭", "F♭", "F", "G♭", "G", "A♭", "A", "B♭", "C♭"],
+    natural: ["C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B"],
+    sharp: ["B♯", "C♯", "D", "D♯", "E", "E♯", "F♯", "G", "G♯", "A", "A♯", "B"],
+  };
+
+  return scale.map((_, k: number) => noteSequences[degree][scale[k]]);
 }
 
 export function normalizePitch(pitch: number) {

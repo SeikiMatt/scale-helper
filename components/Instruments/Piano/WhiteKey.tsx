80780f34daf93087ce293b1534b2b4f0e6bbd78a
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-08-08 03:11:57 -0300

    fix incorrect text color on Piano white keys

--- components/Instruments/Piano/WhiteKey.tsx
+++ components/Instruments/Piano/WhiteKey.tsx
@@ -10,15 +10,15 @@ export default function WhiteKey({
   highlight = 0,
 }: Props) {
   const baseColor = "text-black bg-gray-100 border-gray-200";
-  const rootColor = "bg-purple-400 border-purple-500";
-  const noteColor = "bg-blue-400 border-blue-500";
+  const rootColor = "text-white bg-purple-400 border-purple-500";
+  const noteColor = "text-white bg-blue-400 border-blue-500";
 
   return (
     <div
       className={`
         ${className ? className + " " : ""} ${
         highlight === 1 ? rootColor : highlight === 2 ? noteColor : baseColor
-      } pb-4 border-x-2 border-b-8 drop-shadow rounded-md h-full w-[calc(100%/7)] mx-0.5 flex flex-col-reverse text-center text-white`}
+      } pb-4 border-x-2 border-b-8 drop-shadow rounded-md h-full w-[calc(100%/7)] mx-0.5 flex flex-col-reverse text-center`}
     >
       {note}
     </div>

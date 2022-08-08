9d3e4181a5e54b80c838817f6ebb1c418077a792
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-08-08 00:51:25 -0300

    simplify Guitar component

--- components/Instruments/Guitar/index.tsx
+++ components/Instruments/Guitar/index.tsx
@@ -25,105 +25,88 @@ export default function Guitar({ scale, options }: Props) {
   });
 
   return (
-    <div className="w-full h-64 my-8 px-16">
+    <div className="w-full h-64 my-8 px-4 sm:px-4 lg:px-16 xl:px-48">
       <div className="w-full flex flex-col h-[13.5rem]">
-        {Array(options.strings - 1)
-          .fill(0)
-          .map((x, xlen) => (
-            <div className="flex flex-grow">
-              {Array(options.frets + 1)
-                .fill(0)
-                .map((y, ylen) => (
-                  <div
-                    className={
-                      ylen === 0
-                        ? "border-gray-200 border-r-8 ml-[-2px] flex-grow"
-                        : xlen === options.strings - 2
-                        ? `border-gray-200 border-r-2 border-y-2 flex-grow`
-                        : `border-gray-200 border-r-2 border-t-2 flex-grow`
-                    }
-                  ></div>
-                ))}
-            </div>
-          ))}
+        {noteMatrix.map((_, x) => (
+          <div className="flex flex-grow">
+            {noteMatrix[x].map((_, y) => (
+              <div
+                className={
+                  y === 0
+                    ? "border-gray-200 border-r-8 ml-[-2px] flex-grow"
+                    : x === options.strings - 1
+                    ? `border-gray-200 border-r-2 border-y-2 flex-grow`
+                    : `border-gray-200 border-r-2 border-t-2 flex-grow`
+                }
+              ></div>
+            ))}
+          </div>
+        ))}
       </div>
       <table className="w-full h-64 mt-[-14.8rem] z-10">
         <tbody>
-          {Array(options.strings)
-            .fill(0)
-            .map((_, x) => (
-              <tr className="">
-                {Array(options.frets + 1)
-                  .fill(0)
-                  .map((_, y) => (
-                    <td
-                      className={`text-center w-[calc(100%/${
-                        options.frets + 1
-                      })]`}
-                    >
-                      <button
-                        className={`w-8 h-8 bg-white border-2 border-gray-200 rounded-xl
+          {noteMatrix.map((_, x) => (
+            <tr className="">
+              {noteMatrix[x].map((_, y) => (
+                <td className={`text-center w-[calc(100%/${x})]`}>
+                  <button
+                    className={`w-8 h-8 bg-white border-2 border-gray-200 rounded-xl
                       ${
-                        scale.notes[0] ===
-                        normalizePitch(noteMatrix[options.strings - x - 1][y])
+                        scale.notes[0] === normalizePitch(noteMatrix[x][y])
                           ? " bg-purple-300 border-purple-500"
                           : scale.notes.includes(
-                              normalizePitch(
-                                noteMatrix[options.strings - x - 1][y]
-                              )
+                              normalizePitch(noteMatrix[x][y])
                             ) === true
                           ? " bg-blue-300 border-blue-500"
                           : ""
                       }`}
-                      >
-                        {pitchToNote({
-                          pitch: normalizePitch(
-                            noteMatrix[options.strings - x - 1][y]
-                          ),
-                          degree: scale.degree,
-                        })}
-                      </button>
-                    </td>
-                  ))}
-              </tr>
-            ))}
+                  >
+                    {pitchToNote({
+                      pitch: normalizePitch(
+                        noteMatrix[options.strings - x - 1][y]
+                      ),
+                      degree: scale.degree,
+                    })}
+                  </button>
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="w-full">
         <div className="flex">
-          {Array(options.frets + 1)
-            .fill(0)
-            .map((_, x) => (
-              <div
-                className={`w-full text-center text-gray-600 w-[calc(100%/${
-                  options.frets + 1
-                })]`}
-              >
-                {x}
-                <br />
-                {x === 3
-                  ? "⬤"
-                  : x === 5
-                  ? "⬤"
-                  : x === 7
-                  ? "⬤"
-                  : x === 9
-                  ? "⬤"
-                  : x === 12
-                  ? "⬤ ⬤"
-                  : x === 15
-                  ? "⬤"
-                  : x === 17
-                  ? "⬤"
-                  : x === 19
-                  ? "⬤"
-                  : x === 21
-                  ? "⬤"
-                  : x === 24
-                  ? "⬤"
-                  : ""}
-              </div>
-            ))}
+          {noteMatrix[0].map((_, y) => (
+            <div
+              className={`w-full text-center text-gray-600 w-[calc(100%/${
+                options.frets + 1
+              })]`}
+            >
+              {y}
+              <br />
+              {y === 3
+                ? "⬤"
+                : y === 5
+                ? "⬤"
+                : y === 7
+                ? "⬤"
+                : y === 9
+                ? "⬤"
+                : y === 12
+                ? "⬤ ⬤"
+                : y === 15
+                ? "⬤"
+                : y === 17
+                ? "⬤"
+                : y === 19
+                ? "⬤"
+                : y === 21
+                ? "⬤"
+                : y === 24
+                ? "⬤"
+                : ""}
+            </div>
+          ))}
         </div>
       </div>
     </div>

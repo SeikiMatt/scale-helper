8447b67a4b8d2f1776df1f42155185a6857ad5cc
Matheus Fabris <13111055+SeikiMatt@users.noreply.github.com>
2022-08-10 00:58:43 -0300

    undo ssr block

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import React from "react";
 
-const App = ({ Component, pageProps }: AppProps) => {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-};
-
-export default dynamic(() => Promise.resolve(App), {
-  ssr: false,
-});
+}
